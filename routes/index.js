af194c8492ae32ae60d1b381e6a2ae601a5f0d9f
Aleksandar Bradic <alek.br@gmail.com>
2016-04-07 20:16:07 +0200

    add live stream

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* GET home page. */
-router.get('/belgrade/stream/', function(req, res, next) {
-  res.render('stream', { title: 'Express' });
-});
-
-/* GET home page. */
-router.get('/stream/', function(req, res, next) {
-  res.render('stream', { title: 'Express' });
-});
-
 module.exports = router;
