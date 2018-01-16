b91fed18e9002d79951d495017bbdc3df342baed
Aleksandar Bradic <alek.br@gmail.com>
2018-01-16 11:13:14 -0800

    2018 bootstrap

---
+++ public/belgrade/javascripts/2018.js
@@ -0,0 +1,5 @@
+render = function() {
+}
+
+$( document ).ready(function() {
+})
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/2018', function(req, res, next) {
+  res.render('2018', { title: 'Express' });
+});
+
 
 module.exports = router;
